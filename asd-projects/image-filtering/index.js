70450951ad1af61f79568a1e9dd31c88637d4ce6
gweber101 <gabrielle.weber@nextlevelnola.org>
2022-03-03 14:12:57 +0000

    seven

--- asd-projects/image-filtering/index.js
+++ asd-projects/image-filtering/index.js
@@ -4,9 +4,7 @@ $(document).ready(function(){
     const $display = $('#display');
 
     // Multiple TODOs: Call your apply function(s) here
-    applyFilter(reddify);
-    applyFilter(decreaseBlue);
-    applyFilter(increaseGreenByBlue);
+    applyFilter(reddify, applyFilterNoBackground, applyFilterNoBackground);
 
 
 
@@ -31,6 +29,23 @@ function applyFilter(filterFunction) {
 }
 
 // TODO 7: Create the applyFilterNoBackground function
+function applyFilterNoBackground(filterFunction) {
+    var backgroundColor = [RED];
+    for (var r = 0; r < image.length; r++) {
+        for (var c = 0; c < image[r].length; c++) {
+            if (backgroundColor === filterFunction(rgbNumbers)) { 
+                
+                var rgbNumbers =  rgbStringToArray(rgbString);
+                var rgbString = image[r][c];
+                rgbString = rgbArrayToString(rgbNumbers);
+                image[r][c] = rgbString;
+            }
+        }
+    }
+}
+
+
+
 
 
 // TODO 5: Create the keepInBounds function
@@ -49,8 +64,9 @@ function reddify(arr) {
 function decreaseBlue(arr2) {
 arr2[BLUE] = keepInBounds(arr2[BLUE] - 50);
 }
+
 function increaseGreenByBlue(arr3) {
-arr3[GREEN] = keepInBounds(arr2[BLUE] + arr3[GREEN]);
+arr3[GREEN] = keepInBounds([BLUE] + arr3[GREEN]);
 }
 
 
