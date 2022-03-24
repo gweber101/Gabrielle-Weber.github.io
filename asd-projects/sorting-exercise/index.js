30ccf2cbd58152c1d1dba81e4ab3e43f29b3fe2d
gweber101 <gabrielle.weber@nextlevelnola.org>
2022-03-24 20:11:40 +0000

    sorting completed

--- asd-projects/sorting-exercise/index.js
+++ asd-projects/sorting-exercise/index.js
@@ -29,31 +29,31 @@ async function bubbleSort(array) {
 
 // TODO 3: Implement quickSort
 async function quickSort(array, left, right) {
-    if (right - left > 1){
+    if (array.length > 1) {
         var index = await partition(array, left, right);
-      if (left < index - 1) {
+        if (left < index - 1) {
             await quickSort(array, left, index - 1);
-        }if (right > index) {
-           await quickSort(array, index, right);
+        } if (right > index) {
+            await quickSort(array, index, right);
         }
     }
 }
 
 //TODOs 4 & 5: Implement partition
- async function partition(array, left, right) {
-     var pivot = array[Math.floor((right + left)/2)].value;
-    while(left < right) {
-        while(array[left].value < pivot)
-        left ++;
-        while(array[right].value > pivot)
-        right --;
-        if (left < right) {
+async function partition(array, left, right) {
+    var pivot = array[Math.floor((right + left) / 2)].value;
+    while (left < right) {
+        while (array[left].value < pivot) {
+            left++;
+        } while (array[right].value > pivot) {
+            right--;
+        } if (left < right) {
             swap(array, right, left);
             updateCounter(quickCounter);
             await sleep();
-        } 
-    }return left + 1;
- }
+        }
+    } return left + 1;
+}
 
 // TODO 1: Implement swap
 function swap(array, i, j) {
